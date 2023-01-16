e93a8c5d2536ed87f58a7db3d3b172fcfaa07a17
Andrej Karpathy <andrej.karpathy@gmail.com>
2023-01-16 23:12:25 +0000

    parameter count

--- v2.py
+++ v2.py
@@ -185,6 +185,8 @@ class BigramLanguageModel(nn.Module):
 
 model = BigramLanguageModel()
 m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
