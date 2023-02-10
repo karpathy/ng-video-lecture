66b0c79a058822f1309836b2de6ca47e3031a2bb
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-02-10 17:59:43 +0300

    Teeny-tiny performance improvement
    Instead of indexing positional embeddings we can slice them. It has
    couple of benefits:
    1. Looks cleaner
    2. When indexing - returns new tensor (plus a new tensor each time is
       created with torch.arange comand). In contrast with slicing a view of
       a tensor is returned (basically the same underlying data)

--- gpt.py
+++ gpt.py
@@ -162,7 +162,7 @@ class GPTLanguageModel(nn.Module):
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        pos_emb = self.position_embedding_table.weight[:T] # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
         x = self.blocks(x) # (B,T,C)
         x = self.ln_f(x) # (B,T,C)
